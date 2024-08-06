e876352a79f04b22d4fcd76792660df87bd66901
sacchromycetes <jzhang37@usc.edu>
2024-08-06 08:24:07 +0000

    update

--- scripts/run_digit.sh
+++ scripts/run_digit.sh
@@ -1,19 +1,19 @@
-python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
+python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/hcut_3 --total_part 10 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/blip2-flan-t5-xxl/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
 
-python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
+python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/instructblip-vicuna-13b/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
 
-python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
+python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/llava-1.5-13b-hf/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
 
-python code/run.py --img_path ./data/digits/images/fuyu-8b/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/fuyu-8b/hcut_6 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/fuyu-8b/vcut_6 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
+python code/run.py --img_path ./data/digits/images/fuyu-8b/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/fuyu-8b/hcut_6 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/fuyu-8b/vcut_6 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
 
-python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/hcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
-python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digit/prediction
+python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/contrast_7 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/hcut_3 --total_part 50 --this_part 0 --data_type digit --output_path ./data/digits/prediction
+python code/run.py --img_path ./data/digits/images/Qwen-VL-Chat/vcut_3 --total_part 1 --this_part 0 --data_type digit --output_path ./data/digits/prediction
